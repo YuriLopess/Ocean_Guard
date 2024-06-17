05fe086847f636b60044667b1440cbc26fcb7fc5
YuriLopess <costalopesyuri@gmail.com>
2024-06-17 15:39:29 -0300

    Initial commit.

---
+++ src/js/transitions.js
@@ -0,0 +1,66 @@
+window.sr = ScrollReveal({reset: true});
+
+sr.reveal('.panel-left h2', {origin: "left",
+distance: "50px", 
+duration: 2000,
+reset: false});
+
+sr.reveal('.panel-left h3', {origin: "left",
+distance: "50px", 
+duration: 2000,
+reset: false});
+
+sr.reveal('.panel-left p', {origin: "left",
+distance: "50px", 
+duration: 2500,
+reset: false});
+
+sr.reveal('.panel-left img', {origin: "left",
+distance: "50px", 
+duration: 2000,
+reset: false});
+
+sr.reveal('.panel-right h2', {origin: "right",
+distance: "20px", 
+duration: 2000,
+reset: false});
+
+sr.reveal('.panel-right h3', {origin: "right",
+distance: "20px", 
+duration: 2000,
+reset: false});
+
+sr.reveal('.panel-right p', {origin: "right",
+distance: "20px", 
+duration: 2000,
+reset: false});
+
+sr.reveal('.panel-right img', {origin: "right",
+distance: "20px", 
+duration: 2000,
+reset: false});
+
+sr.reveal('.content-top h2', {origin: "top",
+distance: "20px", 
+duration: 2000,
+reset: false});
+
+sr.reveal('.content-top p', {origin: "left",
+distance: "50px", 
+duration: 2000,
+reset: false});
+
+sr.reveal('.content-bottom', {origin: "left",
+distance: "50px", 
+duration: 2000,
+reset: false});
+
+sr.reveal('.button :first-child', {origin: "left",
+distance: "60px", 
+duration: 2300,
+reset: false});
+
+sr.reveal('.button :nth-child(2)', {origin: "left",
+distance: "50px", 
+duration: 2600,
+reset: false});
